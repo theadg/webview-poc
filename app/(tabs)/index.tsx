3496bf0c79e9783a0d163d6c0a86977d393b2b52
Bernard De Guzman <deguzmanbernardandrew@gmail.com>
2024-10-08 09:13:56 +0800

    fix: downgrade react-native-webview version

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native'
-
-import { HelloWave } from '@/components/HelloWave'
-import ParallaxScrollView from '@/components/ParallaxScrollView'
-import { ThemedText } from '@/components/ThemedText'
-import { ThemedView } from '@/components/ThemedView'
+import { StyleSheet } from 'react-native'
 import WebView from 'react-native-webview'
 
 export default function HomeScreen() {
